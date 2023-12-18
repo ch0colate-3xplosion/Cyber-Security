01418bf9375bae1bb9fd256aa63563446c32a7d3
wh0amI <18145983+ch0colate-3xplosion@users.noreply.github.com>
2023-12-18 15:26:24 -0700

    Delete Proxmox-Vulnlab-Setup/ProxKube directory

--- Proxmox-Vulnlab-Setup/ProxKube/PythonScripts/MassStop.py
+++
@@ -1,9 +0,0 @@
-import os 
-
-cmdDelete = "qm stop "
-VMID =  401
-End_VMID =  459
-
-while VMID <= End_VMID:
-    os.system(cmdDelete + str(VMID))
-    VMID += 1
--- Proxmox-Vulnlab-Setup/ProxKube/PythonScripts/MassVMDestroy.py
+++
@@ -1,9 +0,0 @@
-import os 
-
-cmdDelete = "qm destroy "
-VMID =  600
-End_VMID =  604
-
-while VMID <= End_VMID:
-    os.system(cmdDelete + str(VMID))
-    VMID += 1
